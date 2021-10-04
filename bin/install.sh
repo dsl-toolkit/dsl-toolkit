737fd25ed0034964f04a5c88edd17b53100fb520
Imre Toth <tothimre@gmail.com>
2021-10-04 17:45:26 +0200

    .

--- bin/install.sh
+++ bin/install.sh
@@ -6,8 +6,10 @@ PROJECT_ROOT=$(realpath $SCRIPT_DIR/../)
 NODE_MODULES=$(realpath $PROJECT_ROOT/node_modules)
 echo -- $PROJECT_ROOT -- $NODE_MODULES -- $SCRIPT_DIR
 cd $PROJECT_ROOT
+ls -lah
 if [[ -f "$NODE_MODULES" ]]; then
     npm install && npm run test-clean
 fi
 
+node_modules/.bin/lerna bootstrap
 
