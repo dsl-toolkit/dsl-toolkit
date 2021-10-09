0f3f738a4f12ab1691f3956deb8f95f4644b30ac
Imre Toth <tothimre@gmail.com>
2021-10-09 15:57:18 +0200

    .

--- packages/dsl-framework/src/index.d.ts
+++ packages/dsl-framework/src/index.d.ts
@@ -1,8 +1,16 @@
 
-export default function DslStarter(): typeof DslFramework;
-declare function DslFramework(): typeof DslFrameworkCallback;
-declare function DslFrameworkCallback(): any;
+// function DslStarter(): typeof DslFramework;
+// declare function DslFramework(): typeof DslFrameworkCallback;
+// declare function DslFrameworkCallback(e: number, d: ddd);
 
+import { type } from "os";
+import { setCoreData } from "./core";
+
+export type dslFrameworkStarter = () => dslFramework;
+type dslFramework = ({key: string}) => core;
+type core = (
+    {setCoreData: ()=>void}
+)
 // declare function dslFramework(): any
 // export =  DslFramework = (
 //     {aaa: () => void} ,
---
+++ packages/dsl-framework/src/test.ts
@@ -0,0 +1,12 @@
+const dslFrameworkStarter = require('./');
+
+console.log(dslFrameworkStarter.toString());
+// const dslFramework= dslFrameworkStarter();
+
+// const f = instance() + '2'
+
+// instance.a
+// ccc = instance()
+// ccc()
+// console.log("aaa")
+
