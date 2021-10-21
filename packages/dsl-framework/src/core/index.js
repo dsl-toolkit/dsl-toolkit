2f7454a62a0bb1a967c09b21f28d608f44f37032
Imre Toth <tothimre@gmail.com>
2021-10-21 13:46:34 +0200

    .

--- packages/dsl-framework/src/core/index.js
+++ packages/dsl-framework/src/core/index.js
@@ -3,46 +3,63 @@ const safetyExecutor = require('./detached-executor')
 const container = require('./container/core')
 // const f = function
 const coreFactory = () => {
-  const core = (callback, state = false) => {
+  const core = function me (callback, state = false) {
     let { coreData } = core
-    state = returnStateOrContainer(state)
+    state = state || (function () {
+      // if(coreData.command.has('factory')){
+      //
+      // }
+      return container()
+    }())
     coreData = coreData || container().getFrom(0)
     const callerRaw = function () {
       // parameters
-      const callerArguments = Array.from(arguments)
-      setCommandArguments(callerArguments, state)
-      const data = callerRaw.data = state.getFrom(0)
-      setPromise(coreData, callerRaw, state, callback)
-      const noTriggerEndOfExecution = coreData.command.has('noTriggerEndOfExecution')
-      if (arguments.length && !noTriggerEndOfExecution) {
-        promiseHandler(state, data, callback)
+      if (!callerRaw.called) {
+        callerRaw.called = true
+        return caller
       }
-
-      const evaluateReturnsExpectCaller = ((callerRaw, originalArguments, callback, noTriggerEndOfExecution, state, data) => {
-        state.level++
-        if (!callerRaw.called) {
-          callerRaw.called = true
-          return caller
-        }
-        if (!originalArguments.length && callback && typeof callback === 'function') {
-          return makeCallback(noTriggerEndOfExecution, state, callback, data)
-        }
-        if (!originalArguments.length && !callback) {
-          state.start()
-          return data
-        }
-      })(callerRaw, arguments, callback, noTriggerEndOfExecution, state, data)
+      const callerArguments = Array.from(arguments)
+      if (callerArguments.length) {
+        state.setCommandArguments(callerArguments)
+      }
+      const data = callerRaw.data = state.getFrom(0)
+      if (!coreData.command.has('noPromoises')) {
+        callerRaw.p = require('./caller-promise-factory-factory')(state, callback)
+      }
+      // l(coreData, coreData.command)()
+      const noTriggerEndOfExecution = coreData.command.has('noTriggerEndOfExecution')
       /* istanbul ignore else */
-      if (evaluateReturnsExpectCaller) return evaluateReturnsExpectCaller
+      if (!arguments.length && callback && typeof callback === 'function') {
+        if (!noTriggerEndOfExecution) {
+          clearTimeout(state.timeoutSate)
+        }
+        state.resetMe = true
+        state.start()
+        return callback(RETURN_FROM_CALLBACK, data)
+      }
+      /* istanbul ignore else */
+      if (!arguments.length && !callback) {
+        state.start()
+        return data
+      }
+      /* istanbul ignore else */
+      if (arguments.length && !noTriggerEndOfExecution) {
+        /* istanbul ignore else */
+        if (state.timeoutSate) {
+          clearTimeout(state.timeoutSate)
+        }
+        state.timeoutSate = safetyExecutor(data, callback)
+      }
+      state.level++
 
       return caller
     }
 
     const caller = new Proxy(callerRaw,
       {
-        get (object, prop) {
-          if (getTabooMembers(prop)) {
-            return object[prop]
+        get (obj, prop) {
+          if (prop === 'p' || prop === 'data' || prop === 'apply') {
+            return obj[prop]
           }
           state.setCommandName(prop)
           return caller
@@ -50,40 +67,13 @@ const coreFactory = () => {
         apply (target, thisArg, argumentsList) {
           return target(...argumentsList)
         },
-        set (object, prop, value) {
+        set (obj, prop, value) {
           return Reflect.set(...arguments)
         }
       })
     return caller()
   }
 
-  return giveMyself(core)
-}
-
-module.exports = coreFactory()
-
-function returnStateOrContainer (state) {
-  return state || (function () {
-    // if(coreData.command.has('factory')){
-    return container()
-  }())
-}
-
-function setPromise (coreData, callerRaw, state, callback) {
-  /* istanbul ignore else */
-  if (!coreData.command.has('noPromoises')) {
-    callerRaw.p = require('./caller-promise-factory-factory')(state, callback)
-  }
-}
-
-function setCommandArguments (callerArguments, state) {
-  /* istanbul ignore else */
-  if (callerArguments.length) {
-    state.setCommandArguments(callerArguments)
-  }
-}
-
-function giveMyself (core) {
   core.setCoreData = function (data) {
     this.coreData = data
   }
@@ -91,25 +81,4 @@ function giveMyself (core) {
   return core
 }
 
-function getTabooMembers (prop) {
-  return prop === 'p' || prop === 'data' || prop === 'apply'
-}
-
-function promiseHandler (state, data, callback) {
-  /* istanbul ignore else */
-  if (state.timeoutSate) {
-    clearTimeout(state.timeoutSate)
-  }
-  state.timeoutSate = safetyExecutor(data, callback)
-}
-
-function makeCallback (noTriggerEndOfExecution, state, callback, data) {
-  /* istanbul ignore else */
-  if (!noTriggerEndOfExecution) {
-    clearTimeout(state.timeoutSate)
-  }
-  state.resetMe = true
-  state.start()
-  return callback(RETURN_FROM_CALLBACK, data)
-}
-
+module.exports = coreFactory()
