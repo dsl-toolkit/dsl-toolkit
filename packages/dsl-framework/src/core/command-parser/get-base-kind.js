29e2efa60417de39ba1cb996e1c1f7b4ecfa0e7d
Imre Toth <tothimre@gmail.com>
2021-11-06 05:31:10 +0100

    extractCallbackData extracted form get-base-kind

--- packages/dsl-framework/src/core/command-parser/get-base-kind.js
+++ packages/dsl-framework/src/core/command-parser/get-base-kind.js
@@ -1,41 +1,12 @@
 module.exports = (baseObject, returnObject) => {
   return kind =>
     function () {
-      let baseKindArguments = Array.from(arguments)
-      const argumentsLastIndex =
-        baseKindArguments.length >= 2 ? baseKindArguments.length - 1 : false
-      const argumentsOneBeforeLastIndex =
-        baseKindArguments.length >= 3 ? argumentsLastIndex - 1 : false
-      let trueCaseFunction = false
-      let falseCaseFunction = false
-
-      if (argumentsLastIndex) {
-        if (argumentsOneBeforeLastIndex) {
-          falseCaseFunction = argumentsLastIndex
-            ? typeof arguments[argumentsLastIndex] === 'function'
-              ? arguments[argumentsLastIndex]
-              : false
-            : false
-          trueCaseFunction = argumentsOneBeforeLastIndex
-            ? typeof arguments[argumentsOneBeforeLastIndex] === 'function'
-              ? arguments[argumentsOneBeforeLastIndex]
-              : false
-            : false
-        } else {
-          trueCaseFunction = argumentsLastIndex
-            ? typeof arguments[argumentsLastIndex] === 'function'
-              ? arguments[argumentsLastIndex]
-              : false
-            : false
-          falseCaseFunction = false
-        }
-      }
-      trueCaseFunction &&
-        (() => (baseKindArguments = baseKindArguments.slice(0, -1)))()
-      falseCaseFunction &&
-        (() => (baseKindArguments = baseKindArguments.slice(0, -1)))()
-
-      const havingCaseFunction = !!trueCaseFunction || !!falseCaseFunction
+      var {
+        baseKindArguments,
+        havingCaseFunction,
+        trueCaseFunction,
+        falseCaseFunction
+      } = newFunction(...arguments)
 
       const command = arguments[0]
 
@@ -54,4 +25,47 @@ module.exports = (baseObject, returnObject) => {
         returnValue || (falseCaseFunction && falseCaseFunction())
       }
     }
+}
+function newFunction () {
+  let baseKindArguments = Array.from(arguments)
+  const argumentsLastIndex =
+    baseKindArguments.length >= 2 ? baseKindArguments.length - 1 : false
+  const argumentsOneBeforeLastIndex =
+    baseKindArguments.length >= 3 ? argumentsLastIndex - 1 : false
+  let trueCaseFunction = false
+  let falseCaseFunction = false
+
+  if (argumentsLastIndex) {
+    if (argumentsOneBeforeLastIndex) {
+      falseCaseFunction = argumentsLastIndex
+        ? typeof arguments[argumentsLastIndex] === 'function'
+          ? arguments[argumentsLastIndex]
+          : false
+        : false
+      trueCaseFunction = argumentsOneBeforeLastIndex
+        ? typeof arguments[argumentsOneBeforeLastIndex] === 'function'
+          ? arguments[argumentsOneBeforeLastIndex]
+          : false
+        : false
+    } else {
+      trueCaseFunction = argumentsLastIndex
+        ? typeof arguments[argumentsLastIndex] === 'function'
+          ? arguments[argumentsLastIndex]
+          : false
+        : false
+      falseCaseFunction = false
+    }
+  }
+  trueCaseFunction &&
+    (() => (baseKindArguments = baseKindArguments.slice(0, -1)))()
+  falseCaseFunction &&
+    (() => (baseKindArguments = baseKindArguments.slice(0, -1)))()
+
+  const havingCaseFunction = !!trueCaseFunction || !!falseCaseFunction
+  return {
+    baseKindArguments,
+    havingCaseFunction,
+    trueCaseFunction,
+    falseCaseFunction
+  }
 }
