d2123a4bc27a664ad9e602f0ddaed6e04ad734fc
Imre Toth <tothimre@gmail.com>
2022-04-21 15:33:32 +0200

    lock files updated

--- packages/cowlog/src/index.js
+++ packages/cowlog/src/index.js
@@ -4,7 +4,7 @@ const configParser = require('./app/configParser/configParser')
 module.exports = exports = function (defaultParametersToken) {
   defaultParametersToken = defaultParametersToken || 'default'
   const appContainer =
-        require('./app/container')(Object.assign(configParser('default'), configParser(defaultParametersToken)))
+    require('./app/container')(Object.assign(configParser('default'), configParser(defaultParametersToken)))
 
   return appContainer.get('cowlog')()
 }
