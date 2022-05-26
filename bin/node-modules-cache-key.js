39549427bdbb1777a75a5d4db7a0e0e428b3c1f3
Imre Toth <tothimre@gmail.com>
2022-05-26 10:46:57 +0200

    Publish
     - di-dsl@1.7.41
     - cowlog@1.7.23
     - directory-fixture-provider@1.7.23
     - dsl-framework@1.7.31
     - generic-text-linker@1.7.23
     - refresh-me@1.7.23
     - require-a-lot@1.7.23
     - parenter@1.7.23

--- bin/node-modules-cache-key.js
+++ bin/node-modules-cache-key.js
@@ -1,18 +1,18 @@
 #!/usr/bin/env node
 const {join} = require('path')
-const fs = require('fs')
+// const fs = require('fs')
 
 const getFiles = require('./lib/get-files');
-
 (async ()=>{
   const files = await getFiles(join(__dirname, '..'))
-  const content = files.filter(filename=>!filename.includes('node_modules'))
-  .filter(filename=>filename.endsWith('package.json')).sort()
+  console.log({files});
+  // const content = files.filter(filename=>!filename.includes('node_modules'))
+  // .filter(filename=>filename.endsWith('package.json')).sort()
   const packageJsons = files.filter(filename=>!filename.includes('node_modules'))
   .filter(filename=>filename.endsWith('package.json')).sort()
 
   const objects = packageJsons.map(e=>require('fs').readFileSync(e)).map(e=>JSON.parse(e))
-  const versions = objects.map(e=>e.version).filter(e=>e) 
+  const versions = objects.map(e=>e.version).filter(e=>e)
   console.log(versions)
   const majorVersions = versions.map(e=>{
     const versionArray = e.split('.')
