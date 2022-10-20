e275bd2cdd4e5c3a456515b3dc02608211335c85
Imre Toth <tothimre@gmail.com>
2022-10-20 16:31:44 +0200

    .

--- packages/require-a-lot/src/results-caclulator/index.js
+++ packages/require-a-lot/src/results-caclulator/index.js
@@ -1,14 +1,9 @@
 module.exports = (ralContainer) => {
   let { results } = ralContainer
   const { parameters, infoList, noPackageInfo, requireModuleInstance } = ralContainer
-
   require('./from')(ralContainer)
     .forEach(partialResult => {
-      results = Object.assign(results, partialResult)
-    })
+      results = Object.assign(results, partialResult)})
   require('./hide')(parameters, results)
-
   results = require('demeter-di')(parameters, results, requireModuleInstance, infoList)
-
-  return ({ results, noPackageInfo, infoList })
-}
+  return ({ results, noPackageInfo, infoList })}
