063021c70b4c121c21b5b9691d6f73e4580c0426
Imre Toth <tothimre@gmail.com>
2022-12-15 01:47:06 +0100

    small fixes

--- newPackagesBabel/dsl-framework/src/index.js
+++ newPackagesBabel/dsl-framework/src/index.js
@@ -4,21 +4,6 @@
 /* eslint-disable indent */
 
 const dslFrameworkFactory = require('./core/index.js')
-const dslFrameworkFactoryInitiator = dslFrameworkFactory((e, d) => {
+module.exports = dslFrameworkFactory((e, d) => {
   dslFrameworkFactory.setCoreData(d)
   return dslFrameworkFactory})
-const inBrowser = !!process.browser
-
-if (inBrowser) {window.dslFramework = dslFrameworkFactory}
-
-// eslint-disable-next-line no-undef
-const weHaveAmd = typeof define === 'function' && define.amd
-
-if (weHaveAmd) {
-  // eslint-disable-next-line no-undef
-  define('dsl-framework', [], function () {
-    return dslFrameworkFactoryInitiator})}
-
-(weHaveAmd || inBrowser) && (() => { window.dslFramework = dslFrameworkFactory })()
-
-module.exports = dslFrameworkFactoryInitiator
