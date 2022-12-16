f34e043aa715dc4097936722db24cbbed46f6438
Imre Toth <tothimre@gmail.com>
2022-12-16 07:30:55 +0100

    .

--- newPackagesBabel/demeter-di/src/container-methods/create.js
+++ newPackagesBabel/demeter-di/src/container-methods/create.js
@@ -1,22 +1,26 @@
 import arrify from 'arrify'
 import functionArgumentsGeter  from 'get-function-arguments'
-// import dslFramework from 'dsl-framework'
-// const { parseScript } = require('esprima')
-// const arrayDsl = require('array-dsl')
 
+
+console.log({functionArgumentsGeter},functionArgumentsGeter((a)=>a));
 
 module.exports = (parameters, infoList, results, requireModuleInstance, proxy) => {
   const create = parameters.arguments('create', 'allEntries', [])
   create.length &&
   (() => {
     create.map(createDetails => {
+      console.log('createDetails', createDetails, '----')
       const returnObject = {}
       const factoryDefinition = typeof createDetails[1] === 'string'
         ? requireModuleInstance(createDetails[1])
         : createDetails[1]
       const parameterNames = createDetails[2]
         ? arrify(createDetails[2])
-        : functionArgumentsGeter(factoryDefinition)
+        : (()=>{
+          // console.log('factoryDefinition', factoryDefinition.toString(), '----',functionArgumentsGeter(factoryDefinition))
+          return functionArgumentsGeter(factoryDefinition)})()
+
+      console.log({parameterNames}, '----++++');
       infoList[createDetails[0]] = { head: '*di factory result* ' }
       infoList[createDetails[0] + 'Factory'] = { head: '*di factory* ' }
 
