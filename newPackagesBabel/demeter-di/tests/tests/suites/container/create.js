9dc0e9319d3e9656e138d1e37f3afa373704dfbc
Imre Toth <tothimre@gmail.com>
2022-12-16 07:49:14 +0100

    added create tests

---
+++ newPackagesBabel/demeter-di/tests/tests/suites/container/create.js
@@ -0,0 +1,24 @@
+const assert = require('assert')
+const { create } = require('underscore')
+
+ const containerFactory = require('dsl-framework')()((e, parameters) => require('../../../../src/index.js')(parameters))
+
+ const counter = 0
+
+ describe('create', ()=>{
+    it('tests',()=>{
+      const data = containerFactory
+      .create({
+        fuu: () => 'faa'&&counter++,
+        faa: (bbb) => bbb&&counter++,
+      })
+      create('fua', () => 'faa'&&counter++)
+      .define('bbb', 'ccc')
+      ()
+    const {fuu, faa, fua} = data
+    console.log({fuu, faa,fua});
+    assert(fuu==='faa')
+    assert(fua==='faa')
+    assert (counter===3)
+    })})
+
