0f3a63cbb98c00104bf879dc00fa0af2e08a6b6a
Imre Toth <tothimre@gmail.com>
2022-12-18 01:02:23 +0100

    .

---
+++ newPackagesBabel/demeter-di/tests/tests/suites/container/create-spec.js
@@ -0,0 +1,37 @@
+const assert = require('assert')
+const { create } = require('underscore')
+
+ const containerFactory = require('dsl-framework')()((e, parameters) => require('../../../../src/index.js')(parameters))
+
+ let counter = 0
+
+ describe('create', ()=>{
+    it('tests',()=>{
+      const data = containerFactory
+      .create({
+        fuu: () => {
+          counter++
+          return 'faa'
+        },
+        faa: (bbb) => {
+          counter++
+          return bbb
+        },
+      })
+      .create('fua', () =>{
+        counter++
+        return 'faaa'
+      }
+      )
+      .define('bbb', 'ccc')
+      ()
+    const {fuu, faa, fua} = data
+
+    console.log({fuu, faa,fua})
+    console.log({fua}, 'LLLL')
+
+    assert(fua==='faaa', 'Service fuu is defined and returend correctly. // fuu!="faaa" unfortunately')
+    assert(faa==='ccc')
+    assert (counter===3)
+    })})
+
