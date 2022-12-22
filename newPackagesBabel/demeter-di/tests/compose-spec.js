9bcb56801548d2023b5a9b9df93607cd35a3bc80
Imre Toth <tothimre@gmail.com>
2022-12-22 10:17:04 +0100

    .

--- newPackagesBabel/demeter-di/tests/compose-spec.js
+++ newPackagesBabel/demeter-di/tests/compose-spec.js
@@ -4,12 +4,24 @@ const containerFactory = () =>require('dsl-framework')()((e, parameters) => requ
 
 describe('compose', ()=>{
 
+  describe('basic usage without using the function parameter parsin', ()=>{})
   it('gets the parameter names form an array as third parameter',()=>{
     const data = containerFactory()
     .define('bbb', 'ccc')
     .compose('faa', (bbb) => bbb, ['bbb'])()
 
-    console.log(data.faa);
+    const data2 = containerFactory()
+    .define('bbb', 'ccc')
+    .compose('faa', (c) => c, ['bbb'])()
+
+    assert(data.faa===data2.faa)
+  })
+
+  it('gets undefined parameter names form an array as third parameter',()=>{
+    const data = containerFactory()
+    .compose('faa', (bbb) => bbb, ['ccc'])()
+
+    assert(data.faa===undefined)
   })
 
   it('tests compose and defines together',()=>{
