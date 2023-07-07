76d63594e06df67568eb0341e1971bfd9e84ffe1
Imre Toth <tothimre@gmail.com>
2023-07-07 14:07:20 +0200

    .

--- newPackagesBabel/demeter-di/src/index.d.ts
+++ newPackagesBabel/demeter-di/src/index.d.ts
@@ -1,45 +1,48 @@
-// declare module 'demeter-di' {
-//   type ServiceFunction<T> = (...dependencies: any[]) => T;
+// // declare module 'demeter-di' {
+// //   type ServiceFunction<T> = (...dependencies: any[]) => T;
 
-//   interface Container {
-//     [serviceName: string]: any;
-//   }
+// //   interface Container {
+// //     [serviceName: string]: any;
+// //   }
 
-//   interface ContainerFactory {
-//     define<T>(name: string, value: T): ContainerFactory;
-//     compose<T>(name: string, service: ServiceFunction<T>, dependencies?: string[]): ContainerFactory;
-//     create<T>(name: string, service: ServiceFunction<T>, dependencies?: string[]): ContainerFactory;
-//     (): Container;
-//   }
+// //   interface ContainerFactory {
+// //     define<T>(name: string, value: T): ContainerFactory;
+// //     compose<T>(name: string, service: ServiceFunction<T>, dependencies?: string[]): ContainerFactory;
+// //     create<T>(name: string, service: ServiceFunction<T>, dependencies?: string[]): ContainerFactory;
+// //     (): Container;
+// //   }
 
-//   const containerFactory: ContainerFactory;
+// //   const containerFactory: ContainerFactory;
 
-//   export {
-//     containerFactory
-//   };
-// }
-export type containerFactory = (...args : string[]) => {
-  (): {[key:string]: any};
-  alias: (from:string, to:string) => containerFactory;
-  from: (baseObject: string, tags: string[]) => containerFactory;
-  define: (firstArg:string, secondArg: any) => containerFactory;
-  compose: (
-    firstArg:string, 
-    secondArg: (...args : any[])=>any,
-    secondArgParamContainerNamePointers?: string[]
-  ) => containerFactory;
-  create: (
-    firstArg:string, 
-    secondArg: (...args : any[])=>any,
-    secondArgParamContainerNamePointers?: string[]
-  ) => containerFactory;
-  hide: (tag: string) => containerFactory;
-  log: containerFactory;
-  info: containerFactory;
-  tag: (directory: string)=>containerFactory;
-  information: (index:string, text:string) => containerFactory;
-};
+// //   export {
+// //     containerFactory
+// //   };
+// // }
+// export type containerFactory = (...args : string[]) => {
+//   (): {[key:string]: any};
+//   alias: (from:string, to:string) => containerFactory;
+//   from: (baseObject: string, tags: string[]) => containerFactory;
+//   define: (firstArg:string, secondArg: any) => containerFactory;
+//   compose: (
+//     firstArg:string, 
+//     secondArg: (...args : any[])=>any,
+//     secondArgParamContainerNamePointers?: string[]
+//   ) => containerFactory;
+//   create: (
+//     firstArg:string, 
+//     secondArg: (...args : any[])=>any,
+//     secondArgParamContainerNamePointers?: string[]
+//   ) => containerFactory;
+//   hide: (tag: string) => containerFactory;
+//   log: containerFactory;
+//   info: containerFactory;
+//   tag: (directory: string)=>containerFactory;
+//   information: (index:string, text:string) => containerFactory;
+// };
 
-export type coreFactoryStarter = (require: (requiredPAth:string)=>any) => containerFactory;
+// export type coreFactoryStarter = (require: (requiredPAth:string)=>any) => containerFactory;
 
-export default containerFactory
+// export default containerFactory
+
+export const anyType: any
+export default anyType
